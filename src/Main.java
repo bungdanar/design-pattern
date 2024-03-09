64864f35f772e82872b125998582506b2e5a9db3
Bung Danar <danar.g.putera@gmail.com>
2024-03-09 09:46:40 +0700

    Finish state pattern

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,12 @@
-import memento.Editor;
-import memento.History;
+import state.Canvas;
+import state.EraserTool;
+import state.abuse.Stopwatch;
 
 public class Main {
     public static void main(String[] args) {
-        var editor = new Editor();
-        var history = new History();
-
-        editor.setContent("a");
-        history.push(editor.createState());
-
-        editor.setContent("b");
-        history.push(editor.createState());
-
-        editor.setContent("c");
-        history.push(editor.createState());
-
-        editor.restore(history.pop());
-
-        System.out.println(editor.getContent());
+        var stopwatch = new Stopwatch();
+        stopwatch.click();
+        stopwatch.click();
+        stopwatch.click();
     }
 }
