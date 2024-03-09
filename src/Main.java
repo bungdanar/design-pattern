dd2332d031674c46d848ab1e30838717e6c5da6a
Bung Danar <danar.g.putera@gmail.com>
2024-03-09 18:18:05 +0700

    Finish command pattern

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,23 @@
-import template.TransferMoneyTask;
+import command.*;
+import command.editor.BoldCommand;
+import command.editor.History;
+import command.editor.HtmlDocument;
+import command.editor.UndoCommand;
+import command.fx.Button;
 
 public class Main {
     public static void main(String[] args) {
-        var task = new TransferMoneyTask();
-        task.execute();
+        var history = new History();
+        var document = new HtmlDocument();
+        document.setContent("Hello World");
+
+        var boldCommand = new BoldCommand(document, history);
+        boldCommand.execute();
+        System.out.println(document.getContent());
+
+        var undoCommand = new UndoCommand(history);
+        undoCommand.execute();
+
+        System.out.println(document.getContent());
     }
 }
