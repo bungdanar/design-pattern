377d6d6a41a9fb87dede7c52c2919177f656668f
Bung Danar <danar.g.putera@gmail.com>
2024-03-11 11:25:14 +0700

    Finish chain of responsibility pattern

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,12 @@
-import mediator.ArticlesDialogBox;
+import chainOfResponsibility.*;
 
 public class Main {
     public static void main(String[] args) {
-        var dialog = new ArticlesDialogBox();
-        dialog.simulateUserInteraction();
+        var encryptor = new Encryptor(null);
+        var compressor = new Compressor(encryptor);
+        var logger = new Logger(compressor);
+        var authenticator = new Authenticator(logger);
+        var server = new WebServer(authenticator);
+        server.handle(new HttpRequest("admin", "1234"));
     }
 }
