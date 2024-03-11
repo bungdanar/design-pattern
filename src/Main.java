c283486937fc9b0be77132884bc1de7af16cfb8b
Bung Danar <danar.g.putera@gmail.com>
2024-03-11 17:29:39 +0700

    Finish visitor pattern

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,11 @@
-import chainOfResponsibility.*;
+import visitor.*;
 
 public class Main {
     public static void main(String[] args) {
-        var encryptor = new Encryptor(null);
-        var compressor = new Compressor(encryptor);
-        var logger = new Logger(compressor);
-        var authenticator = new Authenticator(logger);
-        var server = new WebServer(authenticator);
-        server.handle(new HttpRequest("admin", "1234"));
+        var document = new HtmlDocument();
+        document.add(new HeadingNode());
+        document.add(new AnchorNode());
+        document.execute(new HighlightOperation());
+        document.execute(new PlainTextOperation());
     }
 }
