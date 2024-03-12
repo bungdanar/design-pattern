b2284b7a2d79c3d785cbc2de93d6318b4b83693a
Bung Danar <danar.g.putera@gmail.com>
2024-03-12 11:44:22 +0700

    Finish facade pattern

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,8 @@
-import structural.decorator.CloudStream;
-import structural.decorator.CompressedCloudStream;
-import structural.decorator.EncryptedCloudStream;
-import structural.decorator.Stream;
+import structural.facade.NotificationService;
 
 public class Main {
     public static void main(String[] args) {
-        storeCreditCard(new CompressedCloudStream(new EncryptedCloudStream(new CloudStream())));
-    }
-
-    public static void storeCreditCard(Stream stream) {
-        stream.write("1234-1234-1234-1234");
+        var service = new NotificationService();
+        service.send("Hello World", "target");
     }
 }
